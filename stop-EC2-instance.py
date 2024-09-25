7f95ee21f3ce3a34f4b821aca1ece6fd92cd98f3
Ben Saif <raida.benseif@gmail.com>
2024-09-25 09:40:12 +0100

    Update stop-EC2-instance.py

--- stop-EC2-instance.py
+++ stop-EC2-instance.py
@@ -1,29 +1,32 @@
 # In some companies, there is no need to run their EC2 instances 24/7; 
 # they require instances to operate during specific time periods, on working hours, from 8:00 AM in the morning to 5:00 PM in the evening. 
 
-# Stop a specific instance automatically
+# Stop one or many instances automatically
 
 import boto3
 
 def lambda_handler(event, context):
     # Initialize the EC2 client
-    ec2_client = boto3.client('ec2', region_name='ap-south-1') # Replace your Region name
+    ec2_client = boto3.client('ec2', region_name='#########') # Replace your Region name
 
-    # Specify the instance ID of the EC2 instance you want to stop
-    instance_id = 'i-051262059d6f6350a' # Replace your Own Instance ID
+    # Specify the instances ID of the EC2 instances you want to stop
+    instance_id = [
+        '##############',
+        '##############',
+                  ] # Replace your Own Instances ID
 
-    # Stop the EC2 instance
+    # Stop the EC2 instances
     try:
-        response = ec2_client.stop_instances(InstanceIds=[instance_id], DryRun=False)
-        print(f"Stopping EC2 instance {instance_id}...")
+        response = ec2_client.stop_instances(InstanceIds=instance_id, DryRun=False)
+        print(f"Stopping EC2 instances {instance_id}...")
         print(f"Response: {response}")
         return {
             'statusCode': 200,
-            'body': f"EC2 instance {instance_id} is being stopped."
+            'body': f"EC2 instances {instance_id} is being stopped."
         }
     except Exception as e:
-        print(f"Error stopping EC2 instance: {e}")
+        print(f"Error stopping EC2 instances: {e}")
         return {
             'statusCode': 500,
-            'body': f"Error stopping EC2 instance: {str(e)}"
+            'body': f"Error stopping EC2 instances: {str(e)}"
         }
