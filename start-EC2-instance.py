0f9d30a177c68386d83e4a5d9dcfd3d5cd7def48
Ben Saif <raida.benseif@gmail.com>
2024-09-25 09:42:37 +0100

    Update start-EC2-instance.py

--- start-EC2-instance.py
+++ start-EC2-instance.py
@@ -1,7 +1,7 @@
 # In some companies, there is no need to run their EC2 instances 24/7; 
 # they require instances to operate during specific time periods, on working hours, from 8:00 AM in the morning to 5:00 PM in the evening. 
 
-# start a specific instance automatically
+# start one or many instances automatically
 
 import boto3
 
@@ -9,21 +9,24 @@ def lambda_handler(event, context):
     # Initialize the EC2 client
     ec2_client = boto3.client('ec2', region_name='########') # Replace your Region name
 
-    # Specify the instance ID of the EC2 instance you want to start
-    instance_id = '###########' # Replace your Own Instance ID
+    # Specify the instances ID of the EC2 instances you want to start
+    instance_id = [
+        '###########',
+        '###########',
+        ]# Replace your Instances IDs
 
-    # Start the EC2 instance
+    # Start the EC2 instances
     try:
-        response = ec2_client.start_instances(InstanceIds=[instance_id], DryRun=False)
+        response = ec2_client.start_instances(InstanceIds=instance_id, DryRun=False)
         print(f"Starting EC2 instance {instance_id}...")
         print(f"Response: {response}")
         return {
             'statusCode': 200,
-            'body': f"EC2 instance {instance_id} is being started."
+            'body': f"EC2 instances {instance_id} are being started."
         }
     except Exception as e:
-        print(f"Error starting EC2 instance: {e}")
+        print(f"Error starting EC2 instances: {e}")
         return {
             'statusCode': 500,
-            'body': f"Error starting EC2 instance: {str(e)}"
+            'body': f"Error starting EC2 instances: {str(e)}"
         }
